9a6139bc04efc1a967af1ea6bb43ae1890d240f8
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-05-02 20:15:39 +0530

    updated UI

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,7 +12,7 @@ const Experience = () => {
         <hr className="flex-1" />
       </div>
 
-      <div className="flex flex-col md:flex-row p-4 w-[75%] mt-2">
+      <div className="flex flex-col  md:flex-row p-4 w-[75%] mt-2">
         <div className="p-5">
           <h1 className="text-nowrap text-2xl font-medium">
             Frontend Developer
@@ -22,46 +22,33 @@ const Experience = () => {
           <p className="p-2 text-nowrap">June 2023 - July 2023</p>
         </div>
         <div className="border-l-[1px] border-black mr-2 sm:hidden md:block"></div>
-        <ul className="p-5 pt-0 list-disc text-wrap">
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            fugiat quam possimus aspernatur in officiis tempore.
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-            provident odio eaque at numquam obcaecati?
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur accusamus debitis at autem laborum beatae iusto id
-            facere blanditiis.
-          </li>
-        </ul>
+        <p className="lg:mt-4 lg:ml-4">
+          Completed a front-end development internship where I gained hands-on
+          experience in styling web pages using CSS and became comfortable with
+          creating responsive layouts using Bootstrap. The internship also
+          helped me improve my collaboration and communication skills by working
+          closely with team members on real-world projects in a professional
+          environment.
+        </p>
       </div>
       <div className="flex flex-col md:flex-row p-4 w-[75%] mt-2">
         <div className="p-5 sndExp2">
-          <h1 className="text-nowrap text-2xl font-medium">
+          <span className="text-2xl font-medium text-nowrap">
             Software Developer
-          </h1>
+          </span>
           <p className="p-2">Workcohol</p>
           <hr />
           <p className="p-2 text-nowrap">January 2025 - March 2025</p>
         </div>
-        <ul className="p-5 pt-0 list-disc text-wrap ">
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            fugiat quam possimus aspernatur in officiis tempore.
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-            provident odio eaque at numquam obcaecati?
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur accusamus debitis at autem laborum beatae iusto id
-            facere blanditiis.
-          </li>
-        </ul>
+        <p className="lg:mt-4 mr-4">
+          Worked on building a social community platform as part of a full-stack
+          web development internship with Workcohol, Chennai. Developed
+          responsive user interfaces using React, Next.js, and Tailwind CSS, and
+          implemented backend functionality with Python, Django, SQL, and JWT.
+          Also handled state management with Zustand and used GitHub for version
+          control. This experience strengthened my technical skills, teamwork,
+          and understanding of modern web development workflows.
+        </p>
         <div className="border-l-[1px] border-black mr-2 sm:hidden md:block"></div>
         <div className="p-5 md:block sndExp">
           <h1 className="text-nowrap ml-2 text-2xl font-medium">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,25 +50,24 @@ const Projects = () => {
       <div className="flex flex-col md:flex-row gap-4 p-6 w-[75%] text-end">
         <div>
           <h1 className="mr-3 font-extrabold text-xl">
-            Cartify -{" "}
-            <span className="font-light text-nowrap"> E-Commerce App</span>
+            Spend Smart -{" "}
+            <span className="font-light text-nowrap">Expense Tracker App</span>
           </h1>
-          <p className="text-balance p-4">
-            This project features a dynamic eCommerce platform where users can
-            explore, filter, and sort products, select variants (like size), and
-            add items to their cart. Customers can easily complete their
-            purchase by providing a delivery address and choosing from two
-            payment methods: Cash on Delivery or online payments via Stripe and
-            Razorpay gateways.
+          <p className="text-justify p-4">
+            Spend Smart is a responsive and minimalistic expense tracking web
+            app that helps users manage their personal finances by keeping track
+            of their income and expenses in real time. Built using React with
+            the Context API and useReducer for global state management, the app
+            offers a smooth, state-driven UI without the need for external state
+            libraries.
           </p>
           <div className="flex flex-col items-end mr-4">
             <hr className="mt-3 w-[86%] md:w-[73%]" />
 
             <ul className="flex mt-3 gap-x-3 text-end">
               <li>React</li>
-              <li>Tailwind</li>
-              <li>Express</li>
-              <li>Mongo DB</li>
+              <li>CSS</li>
+              <li>Context API</li>
             </ul>
 
             <div className="flex mt-3 gap-x-2">
@@ -82,10 +81,131 @@ const Projects = () => {
           </div>
         </div>
         <img
-          src="/jd.jpg"
+          src="/spend-smart.png"
           alt=""
-          className="max-w-full md:max-w-[35%] h-auto object-contain rounded-3xl"
+          className="object-contain rounded-2xl lg:max-w-xl ring-2"
         />
+      </div>
+
+      <div className="flex items-center w-[72%] gap-2 mt-5 mb-4">
+        <hr className="flex-1" />
+        <h1 className="font-bold text-2xl">Other Projects</h1>
+        <hr className="flex-1" />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5 ">
+        <div className="flex flex-col items-center lg:flex-row gap-4">
+          <div className="rounded-2xl w-[60%] sm:w-auto max-w-lg ring-2 object-cover overflow-hidden">
+            <img src="/foodapp.png" alt="" />
+          </div>
+          <div>
+            <h1 className="font-bold">
+              Dish Delight <span className="font-light"> - Recipe App</span>
+            </h1>
+            <p>
+              A modern web application developed using React, Vanilla CSS, and
+              the Spoonacular API. Dish Delight enables users to discover and
+              explore a wide range of recipes based on ingredients and meal
+              types. The application features a clean, responsive interface and
+              provides detailed cooking instructions, ingredients to enhance the
+              user’s cooking experience.
+            </p>
+            <hr className="border-t mt-2 w-full" />
+            <ul className="flex gap-2">
+              <li>React</li>
+              <li>CSS</li>
+            </ul>
+            <div className="flex gap-2 mt-2">
+              <img src="/github.svg" alt="" className="w-5" />
+              <img src="link1.svg" alt="" className="w-3 hover:scale-120" />
+            </div>
+          </div>
+        </div>
+        <div className=" flex flex-col items-center lg:flex-row gap-4">
+          <div className=" rounded-2xl  w-[60%] sm:w-auto lg:max-w-lg ring-2 object-cover overflow-hidden">
+            <img src="taskhive.png" alt="" />
+          </div>
+          <div>
+            <h1 className="font-bold">
+              Task Hive <span className="font-light">- Todo App </span>
+            </h1>
+            <p>
+              A responsive task management web application built using the MERN
+              stack (MongoDB, Express.js, React, Node.js). TaskHive allows users
+              to create, update, and delete tasks with a clean and intuitive
+              interface. Designed without authentication for simplicity, the app
+              focuses on core CRUD functionality and real-time UI updates,
+              making it ideal for personal productivity and task tracking.
+            </p>
+            <hr className="border-t mt-2 w-full" />
+            <ul className="flex gap-2">
+              <li>React</li>
+              <li>Tailwind CSS</li>
+              <li>Node JS</li>
+              <li>Mongo Db</li>
+            </ul>
+            <div className="flex gap-2 mt-2">
+              <img src="/github.svg" alt="" className="w-5" />
+              <img src="link1.svg" alt="" className="w-3 hover:scale-120" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center lg:flex-row gap-4 mt-4">
+          <div className=" rounded-2xl  w-[60%] sm:w-auto lg:max-w-lg ring-2 object-cover overflow-hidden">
+            <img src="/apod.png" alt="" />
+          </div>
+          <div>
+            <h1 className="font-bold">
+              APOD{" "}
+              <span className="font-light">- Astronomy Pic of the Day</span>
+            </h1>
+            <p>
+              A visually engaging web app that showcases NASA’s Astronomy
+              Picture of the Day. Users can view daily space images along with
+              detailed descriptions, dates, and titles. The app offers a simple
+              and responsive interface, allowing users to explore stunning
+              astronomical content and learn more about space, one day at a
+              time.
+            </p>
+            <hr className="border-t mt-2 w-full" />
+            <ul className="flex gap-2">
+              <li>React</li>
+              <li>CSS</li>
+              <li>Nasa's Open API</li>
+            </ul>
+            <div className="flex gap-2 mt-2">
+              <img src="/github.svg" alt="" className="w-5" />
+              <img src="link1.svg" alt="" className="w-3 hover:scale-120" />
+            </div>
+          </div>
+        </div>
+        <div className=" flex flex-col items-center lg:flex-row gap-4 mt-4 ">
+          <div className=" rounded-2xl  w-[60%] sm:w-auto lg:max-w-lg ring-2 object-cover overflow-hidden">
+            <img src="/car_rental.png" alt="" />
+          </div>
+          <div>
+            <h1 className="font-bold">
+              Car Rental <span className="font-thin">- Landing page</span>
+            </h1>
+            <p>
+              A sleek and responsive landing page designed for a car rental
+              service. It features smooth scroll-based animations to enhance
+              user engagement and a modern, clean layout optimized for all
+              devices. The page highlights key services, vehicle categories, and
+              call-to-action sections, offering an intuitive and visually
+              appealing user experience.
+            </p>
+            <hr className="border-t mt-2 w-full" />
+            <ul className="flex gap-2">
+              <li>React</li>
+              <li>Tailwind CSS</li>
+            </ul>
+            <div className="flex gap-2 mt-2">
+              <img src="/github.svg" alt="" className="w-5" />
+              <img src="link1.svg" alt="" className="w-3 hover:scale-120" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
